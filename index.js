9de5d2fe363b0caba6f10172209eed388f4785c1
Arjunsharma-py <arjunsharma5330@gmail.com>
2023-10-04 10:33:20 +0530

    changed cors

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  methods: "GET,POST,PUT,PATCH,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "http://localhost:5173",
+//   methods: "GET,POST,PUT,PATCH,DELETE",
+//   allowedHeaders: "Content-Type,Authorization",
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
+app.use(cors());
 
 app.use("/api/org", orgRoute);
 app.use("/api/mng", managerRoute);
