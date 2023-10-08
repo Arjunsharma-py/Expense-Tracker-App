de40992e677348b90fffb7593344166164193f5e
Arjunsharma-py <arjunsharma5330@gmail.com>
2023-10-09 03:56:26 +0530

    added environments for production and development

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const authRoute = require("./routes/auth");
 const miniRoute = require("./routes/mini");
 
 const error = require("./middlewares/error");
+const frontend = config.get("frontendURL");
 
 if (!config.get("jwtPrivateKey")) {
   console.log("FATAL ERROR: jwtPrivateKey not provided...");
@@ -32,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
 
 const corsOptions = {
-  origin: "https://expense-tracker-client-theta.vercel.app",
+  origin: frontend,
   methods: "GET,POST,PUT,PATCH,DELETE",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true,
